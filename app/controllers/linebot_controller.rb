2ea239e979d5c71f62cc0c00283e2f335a5b87db
kiriha1203 <mizumoto.takefumi@gmail.com>
2020-09-10 20:50:36 +0900

    validate 削除

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -3,16 +3,6 @@ class LinebotController < ApplicationController
 
   protect_from_forgery with: :null_session
 
-  before_action :validate_signature
-
-  def validate_signature
-    body = request.body.read
-    signature = request.env['HTTP_X_LINE_SIGNATURE']
-    unless client.validate_signature(body, signature)
-      error 400 do 'Bad Request' end
-    end
-  end
-
   def client
     @client ||= Line::Bot::Client.new { |config|
       config.channel_secret = ENV["LINE_CHANNEL_SEACRET"]

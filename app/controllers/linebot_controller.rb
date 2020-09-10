2e107133df834f45bbe79ee22f747815528f27e8
kiriha1203 <mizumoto.takefumi@gmail.com>
2020-09-10 20:43:46 +0900

    add 分離

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -3,6 +3,16 @@ class LinebotController < ApplicationController
 
   protect_from_forgery with: :null_session
 
+  before_action :validate_signature
+
+  def validate_signature
+    body = request.body.read
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless client.validate_signature(body, signature)
+      error 400 do 'Bad Request' end
+    end
+  end
+
   def client
     @client ||= Line::Bot::Client.new { |config|
       config.channel_secret = ENV["LINE_CHANNEL_SEACRET"]
@@ -12,12 +22,6 @@ class LinebotController < ApplicationController
 
   def callback
     body = request.body.read
-
-    signature = request.env['HTTP_X_LINE_SIGNATURE']
-    unless client.validate_signature(body, signature)
-      head :bad_reqest
-    end
-
     events = client.parse_events_from(body)
 
     events.each { |event|
